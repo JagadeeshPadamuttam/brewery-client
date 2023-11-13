5a201006e227eaf091e7dbe8c551ab7b42921a23
JagadeeshPadamuttam <88880051+JagadeeshPadamuttam@users.noreply.github.com>
2023-11-13 20:55:45 +0530

    Update Signup.jsx

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -1,72 +1,4 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import {useNavigate} from "react-router-dom";
-// const SignUpForm = () => {
-//   const [inputs, setInputs] = useState({
-//    username: '',
-//    password: '' });
-// const navigate=useNavigate();
-// const handleSubmit = async (e) => {
-//     e.preventDefault();
 
-//     try {
-//       const response = await axios.post('http://localhost:8080/signup', {
-//         username: inputs.username,
-//         password: inputs.password,
-//       });
-
-//       if (response.data.success) {
-//         console.log('User registered successfully');
-        
-//         // Log the username and password as a JSON object
-//         console.log({ username: inputs.username, password: inputs.password });
-
-//         // Return an object or specific data upon successful registration
-//         return { success: true, message: 'User registered successfully' };
-//       }
-//     } catch (error) {
-//       console.error('Error during signup:', error);
-//     }
-
-//     // If an error occurs or registration is not successful, return an error object
-//     return { success: false, message: 'Error during signup' };
-// };
-
-
-
-
-//   const handleChange = (e) => {
-//     setInputs({ ...inputs, [e.target.name]: e.target.value });
-//   };
-
-//   return (
-//     <form className="form-layout" onSubmit={handleSubmit}>
-//       <h2>SIGN UP</h2>
-//       <label htmlFor="username">Username</label>
-//       <input
-//         type="text"
-//         id="username"
-//         className="form-layout input"
-//         placeholder="Enter your username"
-//         value={inputs.username}
-//         onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-//       />
-//       <label htmlFor="password">Password</label>
-//       <input
-//         type="password"
-//         id="password"
-//         className="form-layout input"
-//         placeholder="Enter your password"
-//         value={inputs.password}
-//         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-//       />
-//       <button type="button" className="form-layout button" onClick={()=>navigate("/login")}>Submit</button>
-      
-//     </form>
-//   );
-// };
-
-// export default SignUpForm;
 
 
 
@@ -89,7 +21,7 @@ navigate("/login")
     console.log('Submitting with Username:', inputs.username, 'and Password:', inputs.password);
 
     try {
-      const response = await axios.post('http://localhost:8080/signup', {
+      const response = await axios.post('https://brewery-uw7j.onrender.com/signup', {
         username: inputs.username,
         password: inputs.password,
       });
